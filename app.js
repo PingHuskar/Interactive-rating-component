330edb9859f12b24f7e2f11e1ef4b5db1ca283bb
PingHuskar <61967318+PingHuskar@users.noreply.github.com>
2022-05-08 23:39:57 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById('card-content-2').style.display = 'none';
+function submit() {
+    document.getElementById('card-content-1').style.display = 'none';
+    document.getElementById('card-content-2').style.display = 'initial';
+}
